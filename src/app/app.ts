18c13cb03a67dedc09493ed46024abb19bf6986b
Charles Hook <charles.hook96@outlook.com>
2025-12-14 11:39:14 -0500

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BoxContentComponent } from './box-content/box-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HeaderComponent, BoxContentComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('flexbox-practice');
---
+++ src/app/box-content/box-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-content',
+  imports: [],
+  templateUrl: './box-content.component.html',
+  styleUrl: './box-content.component.scss',
+})
+export class BoxContentComponent {
+
+}
